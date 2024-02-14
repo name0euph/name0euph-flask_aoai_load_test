ac90d8a764d8adbb16425d714b43f70f79d5825c
gcx001504_jfegr <ryouta-arisaka@jfe-systems.com>
2024-02-14 13:18:48 +0900

    OpenAIのモジュールを使用しないように修正

--- infra/test2/main.tf
+++ infra/test2/main.tf
@@ -33,10 +33,12 @@ locals {
     snet-appint = {
       address_prefixes = "10.0.0.0/24"
       service_name = "Microsoft.Web/serverFarms"
+      service_actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
     },
     snet-apiint = {
       address_prefixes = "10.0.1.0/24"
       service_name = "Microsoft.ApiManagement/service"
+      service_actions = ["Microsoft.Network/virtualNetworks/subnets/join/action","Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
     }
   }
 }
@@ -57,7 +59,7 @@ resource "azurerm_subnet" "appint_subnet" {
   delegation {
     name = "delegation"
     service_delegation {
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+      actions = each.value.service_actions
       name    = each.value.service_name
     }
   }
@@ -79,24 +81,119 @@ locals {
   aoai_location = {
     japaneast = {
       location = "japaneast"
+      model_version = "0613"
     },
-    westus2 = {
+    westus = {
       location = "westus"
+      model_version = "1106"
     }
   }
 }
 
 # Azure OpenAI Serviceを作成
-module "aoai" {
-  source = "../modules/ai"
+# コグニティブサービスアカウントの作成
+resource "azurerm_cognitive_account" "cogac" {
   for_each = local.aoai_location
 
-  env = var.env
-  company = var.company
-  location = each.value.location
-  rg_name = azurerm_resource_group.rg.name
-  vnet_id = azurerm_virtual_network.vnet.id
-  pep_subnet_id = azurerm_subnet.pep_subnet.id
+  # リソース名を aoai-{会社名}-{場所名} とする
+  # 例: aoai-jfest-japaneast
+  name                = "aoai-${var.company}-${each.value.location}"
+  location            = each.value.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  kind                  = "OpenAI"
+  sku_name              = "S0"
+  custom_subdomain_name = "aoai-${var.company}-${each.value.location}"
+
+  #外部ネットワークからのアクセスを拒否
+  public_network_access_enabled = false
+
+  tags = {
+    Environment = var.env,
+    Company     = var.company
+  }
+}
+
+# gpt-35-turboのモデルを作成
+resource "azurerm_cognitive_deployment" "aoai_35-turbo" {
+  for_each = local.aoai_location
+
+  # モデルの名前を定義
+  name = "ChatGPT-35-turbo"
+  cognitive_account_id = azurerm_cognitive_account.cogac[each.key].id
+
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = each.value.model_version
+  }
+  scale {
+    capacity = 10
+    type     = "Standard"
+  }
+  rai_policy_name = "Microsoft.Default"
+
+  depends_on = [
+    azurerm_cognitive_account.cogac,
+  ]
+}
+
+# プライベートDNSゾーンを作成
+resource "azurerm_private_dns_zone" "dns_zone_aoai" {
+  name                = "privatelink.openai.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = var.env,
+    Company     = var.company
+  }
+}
+
+# プライベートDNSゾーンと仮想ネットワークを接続
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_attach_aoai" {
+  name                = "dnsvnet-aoai-${var.env}"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone_aoai.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+
+  depends_on = [
+    azurerm_private_dns_zone.dns_zone_aoai,
+  ]
+}
+
+# Azure OpenAIのプライベートエンドポイントを作成
+resource "azurerm_private_endpoint" "pep_aoai" {
+  for_each = local.aoai_location
+
+  # リソース名を pep-aoai-{会社名}-{リージョン} とする
+  # 例: pep-aoai-jfest-japaneast
+  name                = "pep-aoai-${var.company}-${each.key}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pep_subnet.id
+  custom_network_interface_name = "nic-pep-aoai-${var.company}-${each.key}"
+
+  private_service_connection {
+    name                           = azurerm_cognitive_account.cogac[each.key].name
+    private_connection_resource_id = azurerm_cognitive_account.cogac[each.key].id
+    subresource_names              = ["account"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name = "pepdns-aoai-${each.key}"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns_zone_aoai.id]
+  }
+
+  tags = {
+    Environment = var.env,
+    Company     = var.company
+  }
+
+  depends_on = [
+    azurerm_cognitive_account.cogac,
+  ]
 }
 
 # App Serviceを作成
